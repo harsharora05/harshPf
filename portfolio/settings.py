b158cdad000ae9990f6d32ed63e0d6bd468d7bdd
harsh <harsh.official995@gmail.com>
2023-06-05 23:39:57 +0530

    templates changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
